f5881c3a8af427106f45ac81780f7fe407058055
Ben Simpson <thehoagie@gmail.com>
2023-03-02 10:24:19 -0500

    Add sound effects and critical flashing

--- index.js
+++ index.js
@@ -3,58 +3,57 @@ function initialize() {
   const timeCounterEl = document.querySelector('.time_counter');
   const tempEl = document.querySelector('.temp_chart .progress');
   const rmEl = document.querySelector('.rm_chart .progress');
+  const warningEl = document.querySelector('.warning');
 
   let ammo = 1000;
   let timer = 0;
   let temp = 0;
   let ammoHistory = [];
   let fireInterval;
+  // https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=85337
+  let sound = new Audio('turret-fire.mp3');
+  sound.play();
+  sound.pause();
 
   window.addEventListener('keydown', (evt) => {
     if (evt.keyCode !== 32) {
       return;
     }
 
-    if (fireInterval) {
-      return;
-    }
-
-    fireInterval = startFire();
+    startFire();
   });
+  window.addEventListener('keyup', stopFire);
 
-  window.addEventListener('keyup', (evt) => {
-    clearInterval(fireInterval)
-    fireInterval = null;
-  });
-
-  window.addEventListener('touchstart', () => {
-    if (fireInterval) {
-      return;
-    }
-
-    fireInterval = startFire();
-  });
-
-  window.addEventListener('touchend', () => {
-    clearInterval(fireInterval)
-    fireInterval = null;
-  });
+  window.addEventListener('touchstart', startFire);
+  window.addEventListener('touchend', stopFire);
 
   function startFire() {
-    return setInterval(() => {
-      if ((ammo - 2) >= 0 && temp < 100) {
-        ammo -= 2;
+    if (fireInterval) {
+      return;
+    }
+
+    fireInterval = setInterval(() => {
+
+      if ((ammo - 1) >= 0 && temp < 100) {
+        sound.play();
+        ammo -= 1;
         ammoHistory.unshift(Date.now());
         ammoHistory.unshift(Date.now());
-        timer += 2;
+        timer += 1;
       }
 
-      temp = Math.min(100, temp + 1);
+      temp = Math.min(100, temp + .25);
 
       roundsCounterEl.innerText = ammo;
       timeCounterEl.innerText = (timer / 60).toPrecision(2);
       tempEl.style.bottom = `-${100 - temp}%`;
     }, 10);
+  }
+
+  function stopFire() {
+    clearInterval(fireInterval)
+    fireInterval = null;
+    sound.load();
   }
 
   // handle cooldown
@@ -93,6 +92,19 @@ function initialize() {
     const rm = ammoHistory.filter(ammo => (ammo > Date.now() - 60000)).length;
     rmEl.style.bottom = `-${100 - (rm / 10)}%`;
   }, 1000);
+
+  // critical overlay
+  setInterval(() => {
+    if (ammo < 50) {
+      console.log('here')
+      warningEl.classList.remove('hidden');
+      if (warningEl.classList.contains('flash')) {
+        warningEl.classList.remove('flash');
+      } else {
+        warningEl.classList.add('flash');
+      }
+    }
+  }, 500);
 }
 
 // TODO cool sound effects
